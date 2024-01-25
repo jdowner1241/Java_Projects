98b1779a817bc3d9ba7bd314a3166e7b0b555ffb
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-01-24 19:26:42 -0500

    first

---
+++ Class/intro/HelloWorld/helloworldapp.java
@@ -0,0 +1,25 @@
+package helloworldapp;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+    
+    static String myString = "We are running this snippet of code from the command line";
+    
+
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    int x = 10;
+    int y = 4;
+        
+        System.out.println(myString); // Display the string.
+        System.out.println(x+y); // Display the string.
+    }
+
+}
---
+++ Class/intro/MyJavaApp/MyJavaApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; //built-in Java Library
+
+public class MyJavaApp {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+
+        System.out.print("Enter a Number: ");
+        double x = scan.nextDouble();
+
+        System.out.println("\nOutput:");
+        System.out.println("Name: " + name);
+        System.out.println("Number: " + x);
+
+        System.out.println("\nThis is a test...");
+        System.out.print("Math output: " + Math.round(3.1415));
+    }
+
+}
